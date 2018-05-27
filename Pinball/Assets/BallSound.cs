899e4e5f65dc70c9f696a1c34683ea0d99e4e71a
Dennis <DennisFranke96@gmx.de>
2018-05-27 06:00:58 +0200

    Add: Sound

---
+++ Pinball/Assets/BallSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSound : MonoBehaviour
+{
+    AudioSource audioSource;
+    bool canPlaySound = true;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (canPlaySound)
+        {
+            canPlaySound = false;
+            audioSource.Play();
+            StartCoroutine(enableSound(0.5f));
+        }
+    }
+    
+    IEnumerator enableSound(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        while (!gameObject.activeSelf)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        canPlaySound = true;
+    }
+}
